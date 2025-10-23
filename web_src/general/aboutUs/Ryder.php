7ee2cc2f361ee95bf6364b43cd3505fca50454ba
paulusr13 <paulusr@etown.edu>
2025-10-23 10:28:33 -0400

    about page

---
+++ web_src/general/aboutUs/Ryder.php
@@ -0,0 +1,24 @@
+<?PHP
+
+?>
+
+<head> 
+    <title> Ryder Paulus's About
+        <?PHP
+            require_once "../../classes/Page.php";
+        ?>
+</head>
+<body>
+    <main>
+    <section id="Ryder Paulus's bio">
+        <div class = "bio-title">Ryder Paulus</div>
+        <div id ="bio">
+                I am a Junior International Business major with a double major in Information Systems and concentrations in data analytics and management. 
+                I am the president of the Lifting Club and the coach of the Men's Club Soccer Team. 
+                I am from Bethlehem, PA, and look to work in the Raleigh, NC area after college. 
+                Please add me on LinkedIn :0
+        </div>
+<a href="https://www.linkedin.com/in/rpaulus1326/">Visit my LinkedIn</a>
+
+    </main>
+</body>
