9b256c7bc33c148e35be8eaee0e968bc17c4ba84
PaulDavis <phdav405@gmail.com>
2025-10-23 10:42:42 -0400

    Added buttons to link my profile page and starter page

---
+++ web_src/general/aboutUs/Paul.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>About Paul</title>
+</head>
+
+<BODY>
+<div >
+
+<h1> About Paul </h1>
+
+<a href="../../index.html"><button class="button button2">Go Back!</button></a>
+</div>
+
+</BODY>
+
+</html>
+
