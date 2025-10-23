1590ef13137f909aa22703f402cf85235ccacb91
paulusr13 <paulusr@etown.edu>
2025-10-23 09:12:47 -0400

    page.php

--- web_src/page.php
+++ web_src/page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aya's Chat</title>
+    <title>Demo Page</title>
     <link rel="stylesheet" href="styles.css">
 
 <?php
