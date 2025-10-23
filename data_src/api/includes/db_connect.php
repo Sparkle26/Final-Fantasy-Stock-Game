cd88d080852e8af72a77329f1f70d8e85aa53963
Sparkle26 <zourgania@etown.edu>
2025-10-23 09:47:57 -0400

    fix

--- data_src/api/includes/db_connect.php
+++ data_src/api/includes/db_connect.php
@@ -1,14 +1,15 @@
-<!-- File for connecting to the database -->
 <?php
+// File for connecting to the database
 
-    require_once "db_config.php";
+require_once __DIR__ . '/db_config.php';
 
-    // Create database connection
-    $connection = new mysqli($host, $dbUsername, $dbPassword, $database);
+// Create database connection
+$connection = new mysqli($host, $dbUsername, $dbPassword, $database);
 
-    // Check if the connection was successful
-    if ($connection->connect_error) {
-        die("Connection failed: ".$connection->connect_error);
-    }
+// Check if the connection was successful
+if ($connection->connect_error) {
+    // Use error message that won't break rendered HTML
+    die("Connection failed: " . htmlspecialchars($connection->connect_error));
+}
 
 ?>
