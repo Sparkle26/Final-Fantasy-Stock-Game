0e5518cfa38243a0a56d0501c0c2b10329946dc3
Sparkle26 <zourgania@etown.edu>
2025-10-23 08:34:52 -0400

    adding web credentials

--- data_src/api/includes/db_config.php
+++ data_src/api/includes/db_config.php
@@ -1,1 +1,19 @@
 <!-- File for storing database connection settings  -->
+ <?php
+
+
+global $host, $database, $dbUsername, $dbPassword;
+if($_SERVER["HTTP_HOST"]=="127.0.0.1" || $_SERVER["HTTP_HOST"]=="localhost"){
+    $host = "srv557.hstgr.io";
+    $database = "u413142534_fantasydb";
+    $dbUsername = "u413142534_fantasy";
+    $dbPassword = "StocksR0ck!";
+}else{
+    $host = "srv557.hstgr.io";
+    $database = "u413142534.fantasy";
+    $dbUsername = "u413142534.fantasy";
+    $dbPassword = "StocksR0ck!";
+}
+
+
+?>
