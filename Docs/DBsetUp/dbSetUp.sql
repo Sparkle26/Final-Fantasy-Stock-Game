9630f07a133ead3179ef64c90011b6447dbbdc16
paulusr13 <paulusr@etown.edu>
2025-10-23 09:40:54 -0400

    starting db

--- Docs/DBsetUp/dbSetUp.sql
+++ Docs/DBsetUp/dbSetUp.sql
@@ -1,2 +1,23 @@
 CREATE DATABASE IF NOT EXISTS stocksDB;
 use stocksDB;
+
+CREATE TABLE IF NOT EXISTS user (
+    userID int AUTO_INCREMENT,
+    username VARCHAR(45),
+    password VARCHAR(12),
+    wins INT,
+    losses INT,
+    leagueID INT,
+    stocks VARCHAR(10),
+
+    PRIMARY KEY (userID),
+    CONSTRAINT fk_users_to_league FOREIGN KEY (leagueID) REFERNCES league(leagueID)
+);
+
+CREATE TABLE IF NOT EXISTS league ();
+
+CREATE TABLE IF NOT EXISTS holdings ();
+
+CREATE TABLE IF NOT EXISTS matchups ();
+
+CREATE TABLE IF NOT EXISTS sector ();
