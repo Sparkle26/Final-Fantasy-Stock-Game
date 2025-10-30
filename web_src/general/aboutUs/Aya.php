5b8b1decac81c80c5548f174dc25e27b64206e9d
Sparkle26 <zourgania@etown.edu>
2025-10-30 09:06:25 -0400

    adding info in aya page

---
+++ web_src/general/aboutUs/Aya.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>About Aya</title>
+</head>
+
+<BODY>
+<div >
+
+<h1> A little about me! </h1>
+
+<a href="../../index.html"><button class="button button2">Go Back!</button></a>
+</div>
+
+</BODY>
+
+</html>
+
