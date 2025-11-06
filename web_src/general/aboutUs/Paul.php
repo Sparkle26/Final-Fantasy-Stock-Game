61cafd91e385065bc3478a7d540bb9fbe77c3ed1
PaulDavis <phdav405@gmail.com>
2025-11-06 08:40:52 -0500

    Created and started work on about.css stylesheet

--- web_src/general/aboutUs/Paul.php
+++ web_src/general/aboutUs/Paul.php
@@ -1,17 +1,22 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>About Paul</title>
+    <link rel="stylesheet" href="../../stylesheets/about.css">
 </head>
 
-<BODY>
-<div >
-
-<h1> About Paul </h1>
-
-<a href="../../index.html"><button class="button button2">Go Back!</button></a>
-</div>
-
-</BODY>
+<body>
+    <main>
+        <section id="aboutPage">
+            <div class="aboutHeader-textContainer">
+                <h1> About Paul </h1>
+            </div>
+        </section>
+        <div>
+        <a href="../../index.html"><button class="button button2">Go Back!</button></a>
+        </div>
+    </main>
+</body>
 
 </html>
 
