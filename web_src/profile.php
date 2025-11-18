6a97a633db36fab1dcff9233957239f7af39422a
Sparkle26 <zourgania@etown.edu>
2025-11-18 10:47:28 -0500

    mergin connflict

--- web_src/profile.php
+++ web_src/profile.php
@@ -24,7 +24,7 @@ $user = $result->fetch_assoc();
 
 <!DOCTYPE html>
 <html lang="en">
-
+    
 <header class="site-header">
     <div class="site-title-container">
         <h1 class="site-title">Fantasy Stocks</h1>
@@ -34,7 +34,6 @@ $user = $result->fetch_assoc();
         <ul class="site-nav-list">
             <li><a href="index.html">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
-            <li><a href="leagues.php">League</a></li>
             <li><a href="about.php">About</a></li>
         </ul>
     </nav>
@@ -42,17 +41,24 @@ $user = $result->fetch_assoc();
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="stylesheets/profile.css">
-
+    <title>User Dashboard</title>
+    <link rel="stylesheet" href="profile.css">
 </head>
-
-<div class ="profile-body">
-    <h2>Welcome,<br> <?php echo htmlspecialchars($user['username']); ?>!</h2>
+<body>
+    <header class="site-header">
+  <h1 class="site-title">Fantasy Stocks</h1>
+  <nav class = "site-nav">
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="profile.php">Profile</a></li>
+    <li><a href="about.php">About</a></li>
+  </ul>
+</header>
+    <h2>Welcome, <?php echo htmlspecialchars($user['username']); ?>!</h2>
 
     <p><strong>Wins:</strong> <?php echo $user['wins']; ?></p>
     <p><strong>Losses:</strong> <?php echo $user['losses']; ?></p>
 
     <a href="/web_src/classes/Login/Logout.php">Logout</a>
 </body>
-
 </html>
