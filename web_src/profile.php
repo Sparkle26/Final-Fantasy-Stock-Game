dbcd9f560664d8dedf2309fcb7c53a1ee6fe5d82
paulusr13 <paulusr@etown.edu>
2025-11-20 10:11:50 -0500

    profile

--- web_src/profile.php
+++ web_src/profile.php
@@ -40,22 +40,12 @@ $user = $result->fetch_assoc();
         </ul>
     </nav>
 </header>
-
-<head>
-    <meta charset="UTF-8">
-    <title>User Dashboard</title>
-    <link rel="stylesheet" href="profile.css">
+<head> 
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="stylesheets/styles.css" />
+  <link rel="stylesheet" href="stylesheets/profile.css" />
+  <title>User Profile</title>
 </head>
-<body>
-    <header class="site-header">
-  <h1 class="site-title">Fantasy Stocks</h1>
-  <nav class = "site-nav">
-  <ul>
-    <li><a href="index.html">Home</a></li>
-    <li><a href="profile.php">Profile</a></li>
-    <li><a href="about.php">About</a></li>
-  </ul>
-</header>
     <h2>Welcome, <?php echo htmlspecialchars($user['username']); ?>!</h2>
 
     <p><strong>Wins:</strong> <?php echo $user['wins']; ?></p>
